bb23ea248f301d2cd9043509b8ca586ef1f2b056
Laserzwei <lasernr2@gmail.com>
2019-11-20 17:01:43 +0100

    properly updated to 0.29 included gluecode for 0.28 backwardscompatibility

--- data/scripts/lib/turretgenerator.lua
+++ data/scripts/lib/turretgenerator.lua
@@ -1,23 +1,21 @@
 
-
+local localGameVersion = GameVersion()
 function TurretGenerator.generateSeeded(seed, weaponType, dps, tech, rarity, material)
-
     if weaponType == WeaponType.MiningLaser then
-        --print("Replaced Mining", dps, material.name)
         weaponType = WeaponType.RawMiningLaser
     end
-
     if weaponType == WeaponType.SalvagingLaser then
-        --print("Replaced Salvaging", dps, material.name)
         weaponType = WeaponType.RawSalvagingLaser
     end
 
-    local secured = rand
+    if localGameVersion.major == 0 and localGameVersion.minor >= 29 then
+        return TurretGenerator.generateTurret(Random(seed), weaponType, dps, tech, material, rarity)
+    else
+        local secured = rand
+        TurretGenerator.initialize(seed)
+        local turret = TurretGenerator.generateTurret(rand, weaponType, dps, tech, material, rarity)
+        rand = secured
+        return turret
+    end
 
-    TurretGenerator.initialize(seed)
-    local turret = TurretGenerator.generateTurret(rand, weaponType, dps, tech, material, rarity)
-
-    rand = secured
-
-    return turret
 end
--- data/scripts/server/factions.lua
+++ data/scripts/server/factions.lua
@@ -1,8 +1,8 @@
 
 
-local oldInitPlayer = initializePlayer
+local onlyRawMining_InitPlayer = initializePlayer
 function initializePlayer(player)
-    oldInitPlayer(player)
+    onlyRawMining_InitPlayer(player)
     local cargoupgrade = SystemUpgradeTemplate("data/scripts/systems/startercargo.lua", Rarity(RarityType.Exotic), Random():createSeed())
     player:getInventory():add(cargoupgrade, false)
 
